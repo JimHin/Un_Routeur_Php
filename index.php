94b2ae9187a1db0eb6fe1f0435682bd02e7a9ef1
JimHin <krowstudio@icloud.com>
2020-04-16 01:39:12 +0200

    déclaration d'un formulaire pour envoyer sur la route posts/:id

--- index.php
+++ index.php
@@ -8,7 +8,20 @@ $router = new App\Router\Router($_GET['url']);
 $router->get('/', function(){ echo 'je suis à l\'index';});
 $router->get('/:id', function($id){ echo 'je suis à la page'.$id;});
 $router->get('/posts', function(){ echo 'Tous les posts';});
-$router->get('/posts/:id', function($id){ echo "afficher le post n°".$id;});
+$router->get('/posts/:id', function($id){ 
+?>
+<form action="" method="post">
+    <input type="text" name="name" id="">
+    <button type="submit">Envoyer</button>
+</form>
+<?php
+});
 $router->post('/posts/:id', function($id){ echo "poster le post n°".$id;});
 
 $router->drive_check();
+
+
+
+
+
+
